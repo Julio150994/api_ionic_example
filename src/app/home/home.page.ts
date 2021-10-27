fe10866a878407fa5b1d9659a54fc740905217d6
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2021-10-27 13:22:22 +0200

    Prueba Api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RestService } from '../services/rest.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuarios: any;
 
-  constructor() {}
+  constructor(private restService: RestService) {
+    // Llamamos al login que tiene la Promesa (objeto Promise())
+    // data: se llama la api que ha puesto Raúl para este
+    this.restService.login().then(data => {// devuelve a lo que ha llamado esta función
+      //console.log(data);// para mostrar en consola los datos recibidos
+    });
+  }
 
+  verUsuarios() {
+    this.restService.obtenerUsuarios(this.restService.token)
+    .then(data => {
+        this.usuarios = data;
+        console.log(this.usuarios);
+    });
+  }
 }
